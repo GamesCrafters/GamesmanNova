436a817f2401510135d80f225b2f4d4e0dfd4738
Benjamin Riley Zimmerman <bz931@berkeley.edu>
2024-03-08 15:06:56 -0800

    added a blank test.rs file to db, interface, and solver subdirs. added two tests to test raw data parsing fns in src/database/object/record.rs

---
+++ src/interface/test.rs
@@ -0,0 +1,7 @@
+//! # Database Test Module
+//!
+//! This module provides some unit tests for the Nova interface.
+//!
+//! #### Authorship
+//!
+//! - Benjamin Riley Zimmerman, 3/8/2024 (bz931@berkely.edu)
