22a28b9c804ecb27a6b29644371974e48e197a7c
Benjamin R. Zimmerman <bz931@berkeley.edu>
2024-03-08 18:44:41 -0800

    added testing for parsing floats

--- src/database/test.rs
+++ src/database/test.rs
@@ -19,13 +19,13 @@ mod test {
     fn parse_unsigned_correctness() {
         let data1 = vec![0xDE, 0xAD, 0xBE, 0xEF];
         let expected_parse1: u128 = 0xDEAD_BEEF_0000_0000;
-        assert_eq!(parse_unsigned(&data1, data1.len), expected_parse1)
+        assert_eq!(parse_unsigned(&data1, data1.len), expected_parse1);
         let data2 = vec![0x00, 0x00, 0x0F, 0xF0, 0x0F, 0x0F];
         let expected_parse2: u128 = 0x0000_0FF0_0FF0_0000
-        assert_eq!(parse_unsigned(&data2, data2.len))
+        assert_eq!(parse_unsigned(&data2, data2.len));
         let data3 = vec![0xDE, 0xAD, 0xBE, 0xEF, 0xDE, 0xAD, 0xBE, 0xEF, 0x00];
         let expected_parse3: u128 = 0xDEAD_BEEF_DEAD_BEEF;
-        assert_eq!(parse_unsigned(&data3, 128))
+        assert_eq!(parse_unsigned(&data3, 128));
     }
     
     #[test]
@@ -35,7 +35,7 @@ mod test {
         assert!(result.is_err());
         result = panic::catch_unwind(|| parse_unsigned(&data, 127));
         assert!(result.is_err());
-        data = vec![0xDE, 0xAD, 0xBE, 0xEF, 0xDE, 0xAD, 0xBE, 0xEF]
+        data = vec![0xDE, 0xAD, 0xBE, 0xEF, 0xDE, 0xAD, 0xBE, 0xEF];
         result = panic::catch_unwind(|| parse_unsigned(&data, 127));
         assert!(result.is_err());
     }
@@ -44,26 +44,26 @@ mod test {
     fn parse_signed_correctness() {
         let data1 = vec![0xDE, 0xAD, 0xBE, 0xEF];
         let expected_parse1: i128 = 0xDEAD_BEEF_0000_0000 as i128;
-        assert_eq!(parse_signed(&data1, data1.len), expected_parse1)
+        assert_eq!(parse_signed(&data1, data1.len), expected_parse1);
         let data2 = vec![0x00, 0x00, 0x0F, 0xF0, 0x0F, 0x0F];
         let expected_parse2: i128 = 0x0000_0FF0_0FF0_0000 as i128;
-        assert_eq!(parse_signed(&data2, data2.len))
+        assert_eq!(parse_signed(&data2, data2.len));
         let data3 = vec![0xDE, 0xAD, 0xBE, 0xEF, 0xDE, 0xAD, 0xBE, 0xEF, 0x00];
         let expected_parse3: i128 = 0xDEAD_BEEF_DEAD_BEEF as i128;
-        assert_eq!(parse_signed(&data3, 128))
+        assert_eq!(parse_signed(&data3, 128));
     }
     
     #[test]
     fn parse_string_correctness() {
         let data1 = vec![0x73, 0x75, 0x6D, 0x6D, 0x72, 0x73];
         let expected_parse1: String = "summrs";
-        assert_eq!(parse_string(&data1, data1.len), expected_parse1)
+        assert_eq!(parse_string(&data1, data1.len), expected_parse1);
         let data2 = vec![0x4C, 0x61, 0x4E, 0x61, 0x20, 0x64, 0x33, 0x31, 0x20, 0x72, 0x33, 0x59];
         let expected_parse2: String = "LaNa d31 r3Y";
-        assert_eq!(parse_signed(&data2, data2.len))
+        assert_eq!(parse_signed(&data2, data2.len));
         let data3 = vec![0x49, 0x20, 0x67, 0x65, 0x74, 0x20, 0x24, 0x24, 0x24];
         let expected_parse3: String = "i get $$$";
-        assert_eq!(parse_signed(&data3, data3.len))
+        assert_eq!(parse_signed(&data3, data3.len));
     }
 
     #[test]
@@ -72,6 +72,46 @@ mod test {
         let mut result = panic::catch_unwind(|| parse_unsigned(&data, data.len - 1));
         assert!(result.is_err());
         result = panic::catch_unwind(|| parse_unsigned(&data, data.len + 8));
+        assert!(result.is_err());
+    }
+    
+    #[test]
+    fn parse_f32_correctness() {
+        let data1 = vec![0xDE, 0xAD, 0xBE, 0xEF];
+        let expected_parse1: f32 = -6.25985e+18;
+        assert_eq!(parse_string(&data1, data1.len), expected_parse1);
+        let data2 = vec![0x01, 0x23, 0x45, 0x67];
+        let expected_parse2: f32 = 2.99882e-38;
+        assert_eq!(parse_signed(&data2, data2.len));
+    }
+
+    #[test]
+    fn parse_f32_error_correctness() {
+        let mut data = vec![0xDE, 0xAD, 0xBE, 0xEF];
+        let mut result = panic::catch_unwind(|| parse_unsigned(&data, data.len - 1));
+        assert!(result.is_err());
+        data = vec![];
+        result = panic::catch_unwind(|| parse_unsigned(&data, 32));
+        assert!(result.is_err());
+    }
+    
+    #[test]
+    fn parse_f64_correctness() {
+        let data1 = vec![0xFF, 0x00, 0xFF, 0x00, 0xFF, 0x00, 0xFF, 0x00];
+        let expected_parse1: f64 = -1.71465e+38;
+        assert_eq!(parse_string(&data1, data1.len), expected_parse1);
+        let data2 = vec![0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01];
+        let expected_parse2: f64 = 2.36943e-38;
+        assert_eq!(parse_signed(&data2, data2.len));
+    }
+
+    #[test]
+    fn parse_f64_error_correctness() {
+        let mut data1 = vec![0xFF, 0x00, 0xFF, 0x00, 0xFF, 0x00, 0xFF, 0x00];
+        let mut result = panic::catch_unwind(|| parse_unsigned(&data, data.len - 1));
+        assert!(result.is_err());
+        data = vec![];
+        result = panic::catch_unwind(|| parse_unsigned(&data, 32));
         assert!(result.is_err());
     }
     
