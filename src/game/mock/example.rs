5cea4b1ef81fd2f1828729cb4ec6fe83c186867f
Benjamin Riley Zimmerman <bz931@berkeley.edu>
2024-09-18 15:47:54 -0700

    added example simple utility, general sum acyclic game

--- src/game/mock/example.rs
+++ src/game/mock/example.rs
@@ -159,7 +159,100 @@ pub mod simple_utility {
             pub fn new(
                 store: &'a mut Vec<Node>,
             ) -> Result<AcyclicExampleGame<'a>> {
-                todo!()
+                let mut nodes = vec![
+                    node!(0),
+                    node!(1),
+                    node!(0),
+                    node!(1),
+                    node!(0),
+                    node!(1),
+                    node!(0),
+                    node!(1),
+                    node!(0),
+                    node!(1),
+                    node!(0),
+                    node!(1),
+                    node!(0),
+                    node!(1),
+                    node!(0),
+                    node!(1),
+                    node!(0),
+                    node!(1),
+                    node![
+                        SimpleUtility::LOSE.into(),
+                        SimpleUtility::WIN.into(),
+                    ],
+                    node![
+                        SimpleUtility::WIN.into(),
+                        SimpleUtility::LOSE.into(),
+                    ],
+                    node![
+                        SimpleUtility::WIN.into(),
+                        SimpleUtility::TIE.into(),
+                    ],
+                    node![
+                        SimpleUtility::WIN.into(),
+                        SimpleUtility::TIE.into(),
+                    ],
+                    node![
+                        SimpleUtility::TIE.into(),
+                        SimpleUtility::TIE.into(),
+                    ],
+                    node![
+                        SimpleUtility::WIN.into(),
+                        SimpleUtility::WIN.into(),
+                    ],
+                    node![
+                        SimpleUtility::LOSE.into(),
+                        SimpleUtility::LOSE.into(),
+                    ],
+                ];
+
+                let length = store.len();
+                store.append(&mut nodes);
+                let store: &_ = &store[length..];
+
+                let game = builder::SessionBuilder::new(&ACYCLIC_GAME_NAME)
+                    .edge(&store[0], &store[1])?
+                    .edge(&store[0], &store[3])?
+                    .edge(&store[1], &store[2])?
+                    .edge(&store[1], &store[4])?
+                    .edge(&store[2], &store[5])?
+                    .edge(&store[2], &store[7])?
+                    .edge(&store[3], &store[4])?
+                    .edge(&store[3], &store[6])?
+                    .edge(&store[4], &store[7])?
+                    .edge(&store[4], &store[9])?
+                    .edge(&store[5], &store[6])?
+                    .edge(&store[5], &store[8])?
+                    .edge(&store[6], &store[9])?
+                    .edge(&store[6], &store[11])?
+                    .edge(&store[7], &store[8])?
+                    .edge(&store[7], &store[10])?
+                    .edge(&store[8], &store[11])?
+                    .edge(&store[8], &store[13])?
+                    .edge(&store[9], &store[10])?
+                    .edge(&store[9], &store[12])?
+                    .edge(&store[10], &store[13])?
+                    .edge(&store[10], &store[15])?
+                    .edge(&store[11], &store[12])?
+                    .edge(&store[11], &store[14])?
+                    .edge(&store[12], &store[15])?
+                    .edge(&store[12], &store[17])?
+                    .edge(&store[13], &store[14])?
+                    .edge(&store[13], &store[16])?
+                    .edge(&store[14], &store[17])?
+                    .edge(&store[14], &store[19])?
+                    .edge(&store[15], &store[16])?
+                    .edge(&store[15], &store[18])?
+                    .edge(&store[16], &store[19])?
+                    .edge(&store[16], &store[20])?
+                    .edge(&store[17], &store[21])?
+                    .edge(&store[17], &store[22])?
+                    .start(&store[0])?
+                    .build()?;
+
+                Ok(AcyclicExampleGame { game })
             }
 
             /// Creates a PNG image of the game being represented.
