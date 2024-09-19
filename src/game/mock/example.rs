5ed5f2105016ca37c571f5fcd447fa56937d5840
Benjamin Riley Zimmerman <bz931@berkeley.edu>
2024-09-18 18:08:43 -0700

    added example simple utility, zero sum acyclic game

--- src/game/mock/example.rs
+++ src/game/mock/example.rs
@@ -519,11 +519,68 @@ pub mod simple_utility {
         }
 
         impl<'a> AcyclicExampleGame<'a> {
-            /// TODO
             pub fn new(
                 store: &'a mut Vec<Node>,
             ) -> Result<AcyclicExampleGame<'a>> {
-                todo!()
+                let mut nodes = vec![
+                    node!(0),
+                    node!(1),
+                    node!(1),
+                    node!(1),
+                    node!(0),
+                    node!(0),
+                    node!(0),
+                    node!(0),
+                    node!(0),
+                    node![
+                        SimpleUtility::LOSE.into(),
+                        SimpleUtility::WIN.into(),
+                    ],
+                    node![
+                        SimpleUtility::WIN.into(),
+                        SimpleUtility::LOSE.into(),
+                    ],
+                    node![
+                        SimpleUtility::WIN.into(),
+                        SimpleUtility::LOSE.into(),
+                    ],
+                    node![
+                        SimpleUtility::WIN.into(),
+                        SimpleUtility::LOSE.into(),
+                    ],
+                    node![
+                        SimpleUtility::LOSE.into(),
+                        SimpleUtility::WIN.into(),
+                    ],
+                ];
+
+                let length = store.len();
+                store.append(&mut nodes);
+                let store = &store[length..];
+
+                let game = builder::SessionBuilder::new(&ACYCLIC_GAME_NAME)
+                    .edge(&store[0], &store[1])?
+                    .edge(&store[0], &store[2])?
+                    .edge(&store[0], &store[3])?
+                    .edge(&store[1], &store[4])?
+                    .edge(&store[1], &store[5])?
+                    .edge(&store[2], &store[4])?
+                    .edge(&store[2], &store[6])?
+                    .edge(&store[3], &store[4])?
+                    .edge(&store[3], &store[7])?
+                    .edge(&store[3], &store[8])?
+                    .edge(&store[4], &store[9])?
+                    .edge(&store[4], &store[10])?
+                    .edge(&store[5], &store[11])?
+                    .edge(&store[5], &store[12])?
+                    .edge(&store[6], &store[12])?
+                    .edge(&store[6], &store[13])?
+                    .edge(&store[7], &store[10])?
+                    .edge(&store[8], &store[11])?
+                    .start(&store[0])?
+                    .build()?;
+
+                Ok(AcyclicExampleGame { game })
             }
 
             /// Creates a PNG image of the game being represented.
